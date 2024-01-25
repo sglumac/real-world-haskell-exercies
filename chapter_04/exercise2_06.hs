e2433de26aa28b172e956a0a02b6a403e9a48897
Slaven Glumac <slaven.glumac@gmail.com>
2024-01-25 23:47:32 +0100

    chapter_04/exercise2_06.hs

--- chapter_04/exercise2_06.hs
+++ chapter_04/exercise2_06.hs
@@ -1,4 +1,25 @@
+import Data.Char (isSpace)
+
 myAny :: (a -> Bool) -> [a] -> Bool
 myAny p = foldr step False
   where
     step x result = p x || result
+
+myCycle :: [a] -> [a]
+myCycle xs = foldr (:) (myCycle xs) xs
+
+myWords :: String -> [String]
+myWords = foldr step []
+  where
+    step x []
+      | isSpace x = []
+      | otherwise = [[x]]
+    step x (ys : yss)
+      | isSpace x = [] : ys : yss
+      | otherwise = (x : ys) : yss
+
+myUnlines :: [String] -> String
+myUnlines = foldr step []
+  where
+    step xs [] = xs
+    step xs ys = foldr (:) ('\n' : ys) xs
